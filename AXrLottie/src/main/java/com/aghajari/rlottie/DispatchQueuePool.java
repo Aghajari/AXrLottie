d079ba4bc86e30e1b151eed710b5e570ded10811
SuperFashi <admin@superfashi.com>
2022-08-01 11:47:15 -0400

    Replace `DispatchQueuePool` using built-in `ThreadPoolExecutor`

--- AXrLottie/src/main/java/com/aghajari/rlottie/DispatchQueuePool.java
+++ AXrLottie/src/main/java/com/aghajari/rlottie/DispatchQueuePool.java
@@ -18,98 +18,12 @@
 
 package com.aghajari.rlottie;
 
-import android.os.SystemClock;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-import java.security.SecureRandom;
-import java.util.HashMap;
-import java.util.LinkedList;
-
-import androidx.annotation.UiThread;
-
-class DispatchQueuePool {
-    public static SecureRandom random = new SecureRandom();
-
-    private final LinkedList<DispatchQueue> queues = new LinkedList<>();
-    private final HashMap<DispatchQueue, Integer> busyQueuesMap = new HashMap<>();
-    private final LinkedList<DispatchQueue> busyQueues = new LinkedList<>();
-    private final int maxCount;
-    private int createdCount;
-    private final int guid;
-    private int totalTasksCount;
-    private boolean cleanupScheduled;
-
-    private final Runnable cleanupRunnable = new Runnable() {
-        @Override
-        public void run() {
-            if (!queues.isEmpty()) {
-                long currentTime = SystemClock.elapsedRealtime();
-                for (int a = 0, N = queues.size(); a < N; a++) {
-                    DispatchQueue queue = queues.get(a);
-                    if (queue.getLastTaskTime() < currentTime - 30000) {
-                        queue.recycle();
-                        queues.remove(a);
-                        createdCount--;
-                        a--;
-                        N--;
-                    }
-                }
-            }
-            if (!queues.isEmpty() || !busyQueues.isEmpty()) {
-                DispatchQueue.runOnUIThread(this, 30000);
-                cleanupScheduled = true;
-            } else {
-                cleanupScheduled = false;
-            }
-        }
-    };
-
+class DispatchQueuePool extends ThreadPoolExecutor {
     public DispatchQueuePool(int count) {
-        maxCount = count;
-        guid = random.nextInt();
-    }
-
-    @UiThread
-    public void execute(final Runnable runnable) {
-        final DispatchQueue queue;
-        if (!busyQueues.isEmpty() && (totalTasksCount / 2 <= busyQueues.size() || queues.isEmpty() && createdCount >= maxCount)) {
-            queue = busyQueues.remove(0);
-        } else if (queues.isEmpty()) {
-            queue = new DispatchQueue("DispatchQueuePool" + guid + "_" + random.nextInt());
-            queue.setPriority(Thread.MAX_PRIORITY);
-            createdCount++;
-        } else {
-            queue = queues.remove(0);
-        }
-        if (!cleanupScheduled) {
-            DispatchQueue.runOnUIThread(cleanupRunnable, 30000);
-            cleanupScheduled = true;
-        }
-        totalTasksCount++;
-        busyQueues.add(queue);
-        Integer count = busyQueuesMap.get(queue);
-        if (count == null) {
-            count = 0;
-        }
-        busyQueuesMap.put(queue, count + 1);
-        queue.postRunnable(new Runnable() {
-            @Override
-            public void run() {
-                runnable.run();
-                DispatchQueue.runOnUIThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        totalTasksCount--;
-                        int remainingTasksCount = busyQueuesMap.get(queue) - 1;
-                        if (remainingTasksCount == 0) {
-                            busyQueuesMap.remove(queue);
-                            busyQueues.remove(queue);
-                            queues.add(queue);
-                        } else {
-                            busyQueuesMap.put(queue, remainingTasksCount);
-                        }
-                    }
-                });
-            }
-        });
+        super(0, count, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
     }
 }
